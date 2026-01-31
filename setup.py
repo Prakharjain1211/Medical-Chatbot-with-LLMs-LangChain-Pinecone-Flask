94228b61aa790377b4b19253e015ccb5a9174a8c
Prakhar Jain <Prakharjain1211@gmail.com>
2026-01-31 18:05:29 +0530

    Add requirements and setup configuration for Medical Chatbot

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+  name="Medical_Chatbot",
+  version="0.1.0",
+  author="Prakhar Jain",
+  author_email="prakharjain1211@gmail.com",
+  packages=find_packages(),
+  install_required=[]
+)
